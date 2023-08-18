c6f93b374fe79a0264b41d885e3bf20c66d53fee
Lukas Kucera <lukaskucera94@yahoo.de>
2023-08-18 21:30:55 +0200

    added basket classes

---
+++ Core/Entities/UserBasket.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+
+    public class UserBasket
+    {
+        
+
+        public string Id { get; set; }
+        public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+
+
+
+        public UserBasket(string id)
+        {
+            Id = id;
+
+        }
+
+        public UserBasket()
+        {
+        }
+    }
+}
