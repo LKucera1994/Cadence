900f23b837277fca21a84e65220a55d8bce441f3
Lukas Kucera <lukaskucera94@yahoo.de>
2023-09-16 20:22:08 +0200

    Added Order Aggregate and Config

---
+++ Core/Entities/OrderAggregate/Order.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.OrderAggregate
+{
+    public class Order
+    {
+        public Order()
+        {
+        }
+
+        public Order(string buyerEmail,Address shipToAddress, DeliveryMethod deliveryMethod, IReadOnlyList<OrderItem> orderItems, decimal subtotal)
+        {
+           
+            BuyerEmail = buyerEmail;           
+            ShipToAddress = shipToAddress;
+            DeliveryMethod = deliveryMethod;
+            OrderItems = orderItems;
+            Subtotal = subtotal;
+           
+            
+        }
+
+        public int Id { get; set; }
+
+        public string BuyerEmail { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+        public Address ShipToAddress { get; set; }
+        public DeliveryMethod DeliveryMethod { get; set; }
+
+        public IReadOnlyList<OrderItem> OrderItems { get; set; }
+        public decimal Subtotal { get; set; }
+        public OrderStatus Status { get; set; }
+        public string PaymentIntentId { get; set; }
+
+        public decimal GetTotal()
+        {
+            return Subtotal + DeliveryMethod.Price;
+        }
+    }
+}
---
+++ Infrastructure/Configurations/OrderConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Core.Entities.OrderAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configurations
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.OwnsOne(x => x.ShipToAddress, a =>
+            {
+                a.WithOwner();
+            });
+
+            builder.Property(s => s.Status)
+                .HasConversion(
+                x=> x.ToString(),
+                x => (OrderStatus) Enum.Parse(typeof(OrderStatus), x)
+                );
+
+            builder.HasMany(x => x.OrderItems).WithOne().OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
